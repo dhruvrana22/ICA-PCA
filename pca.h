6583a12c1c8c34bb9964c926726baed2e84cf5a0
Dhruv Rana <dhruvrana2001@gmail.com>
2022-06-23 11:32:21 +0530

    Update pca.h

--- pca.h
+++ pca.h
@@ -9,7 +9,7 @@ namespace wisdom{
 		public:
 		int nrSamples=20000;
 		int nrComp=16;
-		void m1(vector<vector<double>> Data);
+		vector<vector<double>> m1(vector<vector<double>> Data);
 	};
 	void vectods(vector<vector<double>> &Data,vector<double> &testData);
 	void dstovec(qbMatrix2<double> &newX,vector<vector<double>> &result);
