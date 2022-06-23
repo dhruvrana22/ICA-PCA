6a211f72e943e8f2937f21a25f3c8713fc19fb8e
Dhruv Rana <dhruvrana2001@gmail.com>
2022-06-23 18:50:20 +0530

    Update pca.h

--- pca.h
+++ pca.h
@@ -7,11 +7,16 @@ using namespace std;
 namespace wisdom{
 	class PCA{
 		public:
+		//Number of Samples in csv file
 		int nrSamples=20000;
+		//Number of components in csv file
 		int nrComp=16;
+		//Principal Component Analysis of Data
 		void m1(vector<vector<double>> Data,vector<vector<double>> &result);
 	};
+	//Converts 2D vector to 1D vector
 	void vectods(vector<vector<double>> &Data,vector<double> &testData);
+	//Converts qbMatrix2 to 2D vector
 	void dstovec(qbMatrix2<double> &newX,vector<vector<double>> &result);
 }
 #endif
