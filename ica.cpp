2faa398053f0cef0ef4a05593d3fad85ad6d58c4
Dhruv Rana <dhruvrana2001@gmail.com>
2022-06-23 18:47:32 +0530

    Update ica.cpp

--- ica.cpp
+++ ica.cpp
@@ -8,9 +8,13 @@ using namespace itpp;
 using namespace std;
 
 namespace wisdom{
+	//Independent Component Ananlysis using Itpp library
+	//Data-data read from csv file
+	//res-Result of ICA
 	void ICA::m2(vector<vector<double>> Data,vector<vector<double>> &res){
 		float tmp = 0.0;
 		mat X = zeros(nrIC,nrSamples);
+		//Convert 2D vector "Data" to mat "X"
 		vectods(Data,X);
 		Fast_ICA my_fastica(X);
 		my_fastica.set_nrof_independent_components(nrIC);
@@ -19,12 +23,14 @@ namespace wisdom{
 			my_fastica.get_mixing_matrix();
 			my_fastica.get_separating_matrix();
 			mat b=my_fastica.get_independent_components();
+			//Convert mat "b" to 2D vector "res" i.e. Result
 			dstovec(b,res);
 		}
 		else{
 			cout << "ICA:Algorithm failed" << endl;
 		}
 	}
+	//Convert 2D vector to mat
 	void vectods(vector<vector<double>> &Data,mat X){
 		int c=X.rows();
 		int r=X.cols();
@@ -34,6 +40,8 @@ namespace wisdom{
 	 	      	}
 		}
 	}
+	
+	//Convert mat to 2D vector
 	void dstovec(mat b,vector<vector<double>> &res){
 		int r=res.size();
 		int c=res[0].size();
