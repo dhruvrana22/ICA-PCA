e379922c8b91c8c2ff4283b2c4699f35cd7fd23b
Dhruv Rana <dhruvrana2001@gmail.com>
2022-06-23 18:51:30 +0530

    Update utils.h

--- utils.h
+++ utils.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 using namespace std;
+//Read data from a csv file "name" to a 2D vector
 void readData(string name,vector<vector<double>> &Data,int rows,int cols);
+//Save data from a 2D vector to a csv file "name"
 void saveData1(string name,vector<vector<double>> &result);
 #endif
