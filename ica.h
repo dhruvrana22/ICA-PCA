b28a24289d48ec552fd52d38968a8a6d5d8dd664
Dhruv Rana <dhruvrana2001@gmail.com>
2022-06-23 12:15:51 +0530

    Update ica.h

--- ica.h
+++ ica.h
@@ -12,7 +12,7 @@ namespace wisdom{
 		public:
 		int nrSamples = 19000; 
 		int nrIC = 4;
-		vector<vector<double>> m2(vector<vector<double>> Data);
+		void m2(vector<vector<double>> Data,vector<vector<double>> &res);
 	};
 	void vectods(vector<vector<double>> &Data,mat X);
 	void dstovec(mat b,vector<vector<double>> &res);
